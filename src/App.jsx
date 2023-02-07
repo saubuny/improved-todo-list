277d5c4a44593dabec894bb90ef591db1e63cdd4
saubuny <122068609+saubuny@users.noreply.github.com>
2023-02-07 12:47:45 -0500

    L: restarted everything because i didn't plan properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import { useState } from 'react';
 import './styles/App.scss';
-import Todo from './components/Todo';
 
 function App() {
-	const [todos, setTodos] = useState([]);
-	const [text, setText] = useState('');
-
 	return (
 		<>
 			<div className="App">
-				<form className="todo-form">
-					<input
-						className="todo-form__input"
-						type="text"
-						onChange={(e) => setText(e.target.value)}
-					/>
-				</form>
-				<p>{text}</p>
-				{}
+				<p>hi</p>
+				<p>hi</p>
 			</div>
 		</>
 	);
